69ad912cdaefb2e0439c65655c5477451089b34b
DaanMemelink <memelinkd@gmail.com>
2024-03-29 20:58:10 +0100

    Styling update & git files

--- client/src/components/Clock.js
+++ client/src/components/Clock.js
@@ -7,7 +7,7 @@ function Clock({className, runningTime, finishTime}) {
             <div className={`clock ${runningTime ? "" : "timeofday"} ${className}`}>
                 {runningTime ? (
                     <div className="times-container">
-                        <div className="time primary">
+                        <div className={`time primary ${!finishTime ? "full-width" : ""}`}>
                             <AutoTextSize minFontSizePx={100} maxFontSizePx={300}>
                                 {finishTime ? finishTime : runningTime}
                             </AutoTextSize>
